c20a7c9a9552657d8f258c6ead9a18619cbb3829
Nine-e <3287746498@qq.com>
2017-06-22 22:43:30 +0800

    booking 中的数字滑动

--- js/booking.js
+++ js/booking.js
@@ -6,11 +6,58 @@
  */
 $(initPage);
 
+var num=1;
+
 function initPage(){
 	$.getJSON("booking.json",function(data){
 		cntData = data.cnt;
 		var cntHtml = $.templates("#cntTmpl").render(cntData)
 		$(".booking").append(cntHtml);
 
+		ctrlNum();
+		Continue();
 	});
+	
+}
+function Continue(){
+	$(".continue-button").click(function(){
+		num++;
+		console.log(num);
+		/*if(num == 0){
+			$(".cnt-num").css("font-size","20px");
+			$(".cnt-num-first").css("font-size","30px");
+			num++;
+		}
+		console.log(num);
+		if(num == 1){
+			$(".cnt-num").css("font-size","20px");
+			$(".cnt-num-second").css("font-size","30px");
+			num++;
+		}
+		console.log(num);*/
+
+	
+	ctrlNum();
+});
+}
+
+function ctrlNum(){
+	console.log(num);
+	if(num == 1){
+			$(".cnt-num-first").siblings().css("font-size","20px");
+			$(".cnt-num-first").css("font-size","30px");
+		}
+	if(num == 2){
+			$(".cnt-num-second").siblings().css("font-size","20px");
+			$(".cnt-num-second").css("font-size","30px");
+		}
+	if(num == 3){
+			$(".cnt-num-third").siblings().css("font-size","20px");
+			$(".cnt-num-third").css("font-size","30px");
+		}
+	if(num == 4){
+			$(".cnt-num-fourth").siblings().css("font-size","20px");
+			$(".cnt-num-fourth").css("font-size","30px");
+		}
+    
 }
