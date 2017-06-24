2793d8ecf36b20cff2d82696ebf0ecd28311f517
shadowlay <1042944044@qq.com>
2017-06-24 18:12:21 +0800

    common部分头和尾的分开，signup signin forgotpwd引入头和尾

---
+++ js/commonhead.js
@@ -0,0 +1,35 @@
+$(initPage);
+
+function initPage(){
+
+    $.getJSON("../json/commonhead.json",function(data){
+
+        menuData = data.menu;
+        var menuHtml = $.templates("#menuTmpl").render(menuData)
+        $(".commonhead").append(menuHtml);
+
+        headData = data.head;
+        var headHtml = $.templates("#headTmpl").render(headData)
+        $(".commonhead").append(headHtml);
+
+        showMenu();
+        hideMenu();
+
+    });
+}
+
+function showMenu(){
+    $('#showMenu').click(function() {
+        $(".menu").show();
+        $('.massagesHide').show();
+        $("body").css("overflow-y","hidden");
+    });
+}
+
+function hideMenu(){
+    $('#hideMenu').click(function() {
+        $(".menu").hide();
+        $('.massagesHide').hide();
+        $("body").css("overflow-y","scroll");
+    });
+}
