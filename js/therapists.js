d3202ad25aaf0d0a43f9ae41cc99e55eb0e3e8e2
shadowlay <1042944044@qq.com>
2017-06-26 19:37:38 +0800

    therapists 未完成

--- js/therapists.js
+++ js/therapists.js
@@ -8,12 +8,21 @@ $ (document).ready(function() {
         $(".therapists").append(betHtml);
     });
 });
-// $.domReady(function () {
-//     $('p').hyphenate('en-us');
-// });
-// var Hypher = require('hypher'),
-//     english = require('hyphenation.en-us'),
-//     h = new Hypher(english);
-//
-// // returns ['hy', 'phen', 'ation']
-// h.hyphenate('hyphenation');
+
+// function load()
+// {   initPage();
+//     $.getJSON("../json/therapists.json", function(data) {
+//         betData = data.bet;
+//         var betHtml = $.templates("#betTmpl").render(betData);
+//         $(".therapists").append(betHtml);
+//         if($(window).height()>=$(document.body).outerHeight())
+//         {
+//             $('.commonfoot').css('position', 'absolute');
+//             $('.commonfoot').css('width', '100%');
+//             $('.commonfoot').css('bottom','0');
+//         }else
+//         {
+//             $('.commonfoot').css('position', 'relative');
+//         }
+//     });
+// }
