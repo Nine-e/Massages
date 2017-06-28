effd9f707dc3d334cdbbe9606cf3acd7b60286d3
Nine-e <3287746498@qq.com>
2017-06-28 20:09:09 +0800

    home 的 contact 与 faq

--- js/home.js
+++ js/home.js
@@ -1,10 +1,41 @@
+$(initPage);
 
-function load()
-{   initPage();
+function initPage(){
 	$.getJSON("../json/home.json", function(data) {
         homeData = data.home;
         var homeHtml = $.templates("#homeTmpl").render(homeData);
         $(".index").append(homeHtml);
+
+        faqData = data.faq;
+        var faqHtml = $.templates("#faqTmpl").render(faqData);
+        $(".index").append(faqHtml);
+
+        contactData = data.contact;
+        var contactHtml = $.templates("#contactTmpl").render(contactData);
+        $(".index").append(contactHtml);
+
+        /*faqMobile();*/
+    });
+    load();
+    
+
+}
+
+function load()
+{  
+	/*$.getJSON("../json/home.json", function(data) {
+        homeData = data.home;
+        var homeHtml = $.templates("#homeTmpl").render(homeData);
+        $(".index").append(homeHtml);
+
+        faqData = data.faq;
+        var faqHtml = $.templates("#faqTmpl").render(faqData);
+        $(".index").append(faqHtml);
+
+        contactData = data.contact;
+        var contactHtml = $.templates("#contactTmpl").render(contactData);
+        $(".index").append(contactHtml);*/
+
         if($(window).height()>=$(document.body).outerHeight())
 		{
 		   $('.commonfoot').css('position', 'absolute');
@@ -14,9 +45,18 @@ function load()
 		{
 		   $('.commonfoot').css('position', 'relative');
 		} 
-		});
+		/*});*/
 	/*window.onresize = function(){
         var home = document.getElementById("home");
         home.style["z-index"] = 1;
     }*/
 }
+/*function faqMobile() {
+	$('.answer').show();
+	if($(window).width()<=750 ){
+		$('.answer').hide();
+		console.log("1");
+	}
+	console.log("2");
+}*/
+/*&& (window.orientation==180||window.orientation==0)*/
