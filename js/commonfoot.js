e89c0ed3c556e9131005eb589e28de65d7f9a9bd
Yinxia Zhang <274581843@qq.com>
2017-06-30 11:15:35 +0800

    ...

--- js/commonfoot.js
+++ js/commonfoot.js
@@ -1,10 +1,10 @@
 
-function initPage(){
+
     $.getJSON("../json/commonfoot.json",function(data){
         footData = data.foot;
         var footHtml = $.templates("#footTmpl").render(footData)
         $(".commonfoot").append(footHtml);
    	});  
-}
+
 	/* Act on the event */
 
--- js/therapists.js
+++ js/therapists.js
@@ -10,20 +10,11 @@
 // });
 
 function load()
-{   initPage();
+{ 
     $.getJSON("../json/therapists.json", function(data) {
         betData = data.bet;
         var betHtml = $.templates("#betTmpl").render(betData);
         $(".therapists").append(betHtml);
-        if($(window).height()>=$(document.body).outerHeight())
-        {
-            $('.commonfoot').css('position', 'absolute');
-            $('.commonfoot').css('width', '100%');
-            $('.commonfoot').css('bottom','0');
-        }else
-        {
-            $('.commonfoot').css('position', 'relative');
-        }
         jQuery(function ($) {
             $('.message').hyphenate('en-us');
         });
