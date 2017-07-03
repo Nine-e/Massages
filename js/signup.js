441de8d0ff2c9bd2595cbeb0619f0064d2ee0f09
Yinxia Zhang <274581843@qq.com>
2017-07-03 09:04:45 +0800

    000

--- js/signup.js
+++ js/signup.js
@@ -8,7 +8,7 @@
 
 
 function load()
-{   initPage();
+{  
     $.getJSON("../json/signup.json", function(data) {
         betData = data.bet;
         var betHtml = $.templates("#betTmpl").render(betData);
@@ -24,3 +24,39 @@ function load()
         }
     });
 }
+function ToSignUp()
+{
+    var firstname=$('.firstnin').val(),
+        lastname=$('.lastnin').val(),
+        email=$('.emailin').val(),
+        mobile=$('.mobilein').val(),
+        password=$('.passwordin').val();
+    if(firstname&&lastname&&email&&mobile&&password)
+    {
+        $.ajax(
+            {
+                type: "GET", //提交方式  
+                url: "http://localhost:8080/phpbin/signup.php?id="+5, //路径  
+                data: {
+                    "firstname": firstname,
+                    "lastname":lastname,
+                    "email":email,
+                    "mobile":mobile,
+                    "password":password
+                },
+                 //数据，这里使用的是Json格式进行传输  
+                success: function(result) { //返回数据根据结果进行相应的处理  
+                    if (result) {
+                       alert("yeah");
+                    } else {
+                        alert("No");
+                    }
+                }
+
+            }
+        );
+    }
+
+}
+$('body').delegate('.signupbtn','click', ToSignUp);
+
